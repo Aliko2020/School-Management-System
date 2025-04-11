5213b5eb5e5e3f225854908fe1d55494cf467cc9
Amos Aliko <alikoamos7@gmail.com>
2025-04-11 03:11:42 +0000

    database connections and test

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,26 @@
 // backend/server.js
+require('dotenv').config(); 
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000
+const pool = require("./database")
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running");
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users JOIN students ON users.user_id = students.user_id');
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error fetching users:', err);
+    res.status(500).send('Server error');
+  }
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
